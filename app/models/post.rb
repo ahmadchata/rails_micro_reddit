492437dcbe384ac0b62033c00d2536bfc9cb0c00
Amita <sardana.amita2@gmail.com>
2020-10-01 19:30:41 +0530

    Add association between user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :description, presence: true
+  validates :description, length: { minimum: 25, maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-	validates :username, :email, :password, presence: true, uniqueness: true
-	validates :username, length: { in: 2..10 }
-	validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 8 }
+  has_many :posts
+
+  validates :username, :email, :password, presence: true, uniqueness: true
+  validates :username, length: { in: 2..10 }
+  validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
 end
