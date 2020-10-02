ab06407f24fd713f21151086533f2c0fd23de8f9
Amita <sardana.amita2@gmail.com>
2020-10-02 19:33:34 +0530

    Update validation as per the review

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  validates :username, :email, :password, presence: true, uniqueness: true
+  validates :username, :email, presence: true, uniqueness: true
   validates :username, length: { in: 2..10 }
-  validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 8 }
+  validates :email, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
 end
